0cb89b58fce335f93db6a881571b953949f9d3bf
Ashanaur <ashanour009@gmail.com>
2019-04-07 23:24:09 -0600

    database class created

---
+++ Class/Database.php
@@ -0,0 +1,26 @@
+<?php
+    class Database{
+        protected $host = 'localhost';
+        protected $user = 'root';
+        protected $pass = '';
+        protected $db = 'hostel';
+        protected $con;
+        protected $result;
+        protected $sql;
+
+        
+        /**
+         * This is a construct method use for database connection..
+         */
+        public function __construct(){
+            if(!isset($this->con)){
+                $this->con = new mysqli($this->host,$this->user,$this->pass,$this->db);
+                if(!$this->con){
+                    echo 'error';
+                }
+            }
+            return $this->con;
+        }
+    }
+
+?>
